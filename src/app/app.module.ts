f18eb5e8248c02dbe3bcbecc6c67c25f618c21a0
pawan999333 <pawansharma19feb@gmail.com>
2025-02-13 18:33:59 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TemplateComponent } from './template/template.component';
+import { FormsModule } from '@angular/forms';
+import { QuillModule } from 'ngx-quill';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    QuillModule.forRoot({})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/template/template.component.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit } from '@angular/core';
+import jsPDF from 'jspdf';
+import * as html2pdf from 'html2pdf.js';
+
+import html2canvas from 'html2canvas';
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styleUrls: ['./template.component.css']
+})
+export class TemplateComponent implements OnInit {
+  url: any;
+  upload: boolean=false;
+  username:any="Pawan Kumar Sharma";
+  address:any='plot no. 17, Alwar,Rajasthan 301001';
+  gmail:any='pawan@email.com';
+  mobile:any='1111111111';
+  summary:any='Results-oriented Engineering Executive with a proven track record of optimizing project outcomes. Skilled in strategic project management and team leadership. Seeking a challenging executive role to leverage technical expertise and drive engineering excellence.';
+  experience:any='Implemented cost-effective solutions, resulting in a 20% reduction in project expenses.Streamlined project workflows, enhancing overall efficiency by 25%.Led a team in successfully delivering a complex engineering project on time and within allocated budget.'
+  company:any='google';
+  skills:any=['Java, Angular, HTML, Css, Bootstrap, Nodejs'];
+  certifications:any=[
+   '1. Certified Project Management (PMP)',
+   '2. Udemy Angular Certification'
+  ];
+  certificationsString: string = this.certifications.join('\n');
+
+  fields: Array<{ company: any, experience: any }> = [
+    { company: 'google', experience: 'Implemented cost-effective solutions, resulting in a 20% reduction in project expenses. Streamlined project workflows, enhancing overall efficiency by 25%. Led a team in successfully delivering a complex engineering project on time and within allocated budget.' },
+
+  ];
+  educations: Array<{ school: any, des: any }> = [
+    { school: 'Master of Science in Mechanical Engineering', des: 'University of Engineering and Technology' },
+
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log(this.fields);
+  }
+  addField() {
+    this.fields.push({ company: 'Microsoft', experience: 'Implemented cost-effective solutions, resulting in a 20% reduction in project expenses.Streamlined project workflows, enhancing overall efficiency by 25%.Led a team in successfully delivering a complex engineering project on time and within allocated budget.' });
+  }
+
+  updateCertifications(value: string) {
+    this.certifications = value.split('\n');
+  }
+  addEducation(){
+    this.educations.push(    { school: 'Master of Science in Mechanical Engineering', des: 'University of Engineering and Technology' },
+    )
+  }
+  onFileChanged(e: Event) {
+    this.upload=true;
+    let reader = new FileReader();
+    const input = e.target as HTMLInputElement;
+    if(input?.files && input.files.length > 0) {
+      let file = input.files[0];
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.url = reader.result;
+      };
+    }
+  }
+
+  downloadPDF() {
+    const element = document.getElementById('pdf-content');
+    if(element){
+    const buttons = element.querySelectorAll('button');
+    buttons.forEach(button => button.classList.add('hidden'));
+
+    const options = {
+      margin: [2,0,0,0],
+      filename: 'template.pdf',
+      image: { type: 'jpeg', quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+    };
+
+    html2pdf().from(element).set(options).save().finally(() => {
+      buttons.forEach(button => button.classList.remove('hidden'));
+    });
+  }
+  }
+}
